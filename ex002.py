3b6300dd539ecde08f2f2c33053dabbf97eb9b03
jpedograu <ultrasenha@gmail.com>
2023-09-22 14:26:23 -0300

    atv2
    atv2

---
+++ ex002.py
@@ -0,0 +1,6 @@
+aqui e feito o calculo da soma de dois numeros
+
+#print("e resultado entre:",v1, 'e',v2, 'e',s)
+print("o valor entre {} e o resultado e{}".format(v1,v2,s))
+print("o valor entre {} e o resultado e{}".format(v1,v2,s))
+print('teste')
